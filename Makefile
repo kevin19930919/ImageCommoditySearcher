d5daeafbcaa6ef113d86363f04130ee13ed27124
kevin_tsai <kevin19930919@gmail.com>
2023-07-25 16:25:58 +0800

    feat: keep web service part for different usage

--- Makefile
+++ Makefile
@@ -1,2 +1,22 @@
+# Replace 'your-aws-account-id' with your AWS account ID
+AWS_ACCOUNT_ID = 561307077992
+# Replace 'your-region' with the AWS region where your ECR repository is located
+AWS_REGION = ap-southeast-1
+# Replace 'image-commodity-searcher' with the name of your ECR repository
+ECR_REPO = kevintsai
+
+# Build the Docker image
 build:
-	docker build -t image-commodity-searcher:latest .
+	docker build -t $(ECR_REPO):latest .
+
+# Tag the Docker image with ECR repository URI
+tag:
+	docker tag $(ECR_REPO):latest $(AWS_ACCOUNT_ID).dkr.ecr.$(AWS_REGION).amazonaws.com/$(ECR_REPO):latest
+
+# Authenticate Docker to your ECR
+login:
+	aws ecr get-login-password --region $(AWS_REGION) | docker login --username AWS --password-stdin $(AWS_ACCOUNT_ID).dkr.ecr.$(AWS_REGION).amazonaws.com
+
+# Push the Docker image to ECR
+push: login tag
+	docker push $(AWS_ACCOUNT_ID).dkr.ecr.$(AWS_REGION).amazonaws.com/$(ECR_REPO):latest
---
+++ web_version.py
@@ -0,0 +1,58 @@
+from PIL import Image
+from image_commodity_searcher.image_caption import ImageCaptionHandler
+from image_commodity_searcher.keyword_generator import KeywordGenerator
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import *
+import os
+import configparser
+from image_commodity_searcher.util.keyword_parser import parse
+
+secrets = configparser.ConfigParser()
+secrets.read('./image_commodity_searcher/secret.ini')
+CHANNEL_SECRET = secrets['DEFAULT']['CHANNEL_SECRET']
+CHANNEL_ACCESS_TOKEN = secrets['DEFAULT']['CHANNEL_ACCESS_TOKEN']
+OPENAI_API_KEY = secrets['DEFAULT']['OPENAI_API_KEY']
+
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
+
+
+@app.route("/search-commodity", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=ImageMessage)
+def handle_message(event):
+    message_content = line_bot_api.get_message_content(event.message.id)
+    filename = f'{event.message.id}.jpg'
+    with open(filename, 'wb') as fd:
+        fd.write(message_content.content)
+    
+    image = Image.open(filename)
+    image_caption_handler = ImageCaptionHandler()
+
+    caption = image_caption_handler.generate_caption(image)
+
+    keyword_generator =  KeywordGenerator(key=OPENAI_API_KEY)
+    result = parse(keyword_generator.generate(caption))
+
+    shopee_link = "https://shopee.tw/search?keyword=" + result
+    print(shopee_link)
+        
+    os.remove(filename)    
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=shopee_link))
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=1219)
